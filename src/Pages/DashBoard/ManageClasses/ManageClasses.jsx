9ad241fe2e315197e38586337ce9a3b8293c7fe2
Monir <hossinmonir681@gmail.com>
2023-06-16 09:53:37 +0600

    manage classes table updated

--- src/Pages/DashBoard/ManageClasses/ManageClasses.jsx
+++ src/Pages/DashBoard/ManageClasses/ManageClasses.jsx
@@ -2,58 +2,62 @@ import React from 'react';
 import useAllClasses from '../../../hook/useAllClasses';
 
 const ManageClasses = () => {
-    const [allClasses, isLoading, refetch] = useAllClasses();
-    
-    return (
-        <div>
-            <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-         
-        </th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-     
-      {
-        allClasses.map(item =><tr key={item._id}>
-            <th>
-              
-            </th>
-            <td>
-              <div className="flex items-center space-x-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img src={item.cImage} alt="Avatar Tailwind CSS Component" />
+  const [allClasses, isLoading, refetch] = useAllClasses();
+
+  return (
+    <div className='w-full'>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Class Name</th>
+              <th>Instructor</th>
+              <th>Instructor Email</th>
+              <th>Seats</th>
+              <th>Price</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {
+              allClasses.map((item,index) => <tr key={item._id}>
+                <th>{index+1}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={item.cImage} alt="Avatar Tailwind CSS Component" />
+                      </div>
+                    </div>
                   </div>
-                </div>
-              </div>
-            </td>
-            <td>{item.cName}</td>
-            <td>{item.iName}</td>
-            <td>{item.iEmail}</td>
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-          </tr>)
-      }
-     
-     
-     
-    </tbody>
-   
-    
-  </table>
-</div>
-        </div>
-    );
+                </td>
+                <td>{item.cName}</td>
+                <td>{item.iName}</td>
+                <td><small>{item.iEmail}</small></td>
+                <td>{item.aSeats}</td>
+                <td>${item.price}</td>
+                <td>{item.status}</td>
+               
+                  <td><button className="btn btn-ghost btn-xs">Approve</button></td>
+                 <td> <button className="btn btn-ghost btn-xs">Deny</button></td>
+                 <td> <button className="btn btn-ghost btn-xs">Feedback</button></td>
+               
+              </tr>)
+            }
+
+
+
+          </tbody>
+
+
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageClasses;
